122e8a1aacb2a9dc403592d8713b731fa9dc422f
Adam Lipian <adamlipian2723@gmail.com>
2024-12-06 01:24:33 +0100

    fix: update return type in median_split

--- decision_tree/split_method.py
+++ decision_tree/split_method.py
@@ -1,6 +1,7 @@
 from pandas import DataFrame
+from typing import Tuple
 
-def median_split(samples: DataFrame, attribute: str) -> list[DataFrame]:
+def median_split(samples: DataFrame, attribute: str) -> list[Tuple[DataFrame, list[float]]]:
     median = samples[attribute].median()
     left_subset = samples[samples[attribute] <= median], [-float("inf"), median]
     right_subset = samples[samples[attribute] > median], [median, float("inf")]
